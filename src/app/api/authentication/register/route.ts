3d4d1465afd1e84574b0b89a3cfd6f5645438b16
DanysGuevara <ga19014@ues.edu.sv>
2024-07-04 02:35:43 -0600

    feat: delete function logins the register with route.ts

--- src/app/api/authentication/register/route.ts
+++ src/app/api/authentication/register/route.ts
@@ -44,39 +44,6 @@ export async function POST(req: NextRequest) {
   }
 }
 
-export async function Login(req: NextRequest) {
-  const { username, password } = await req.json();
-
-  const client = await queryConnection();
-
-  try {
-    const loginQuery = `
-      SELECT * FROM logins WHERE username = $1
-    `;
-    const loginResult = await client.query(loginQuery, [username]);
-
-    if (loginResult.rows.length === 0) {
-      return NextResponse.json({ message: 'Credenciales inválidas' }, { status: 401 });
-    }
-
-    const user = loginResult.rows[0];
-    const isPasswordValid = bcrypt.compareSync(password, user.password);
-
-    if (!isPasswordValid) {
-      return NextResponse.json({ message: 'Credenciales inválidas' }, { status: 401 });
-    }
-
-    const token = jwt.sign({ uuid: user.uuid }, 'tu_secreto', { expiresIn: '1h' });
-
-    return NextResponse.json({ token }, { status: 200 });
-  } catch (error: any) {
-    return NextResponse.json({ message: 'Error al iniciar sesión', error: error.message }, { status: 500 });
-  } finally {
-    client.release();
-  }
-}
-
-
 export function GET(res: NextResponse) {
     return NextResponse.json({ message: 'Hello world' }, { status: 200 });
 }
